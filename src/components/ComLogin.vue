b40db88f29da460bc5b0e73d690b6c9fafc728c0
nggiabao2004 <nguyengiabao022004@gmail.com>
2024-11-20 11:06:44 +0700

    fix ComLogin and ComRegister

--- src/components/ComLogin.vue
+++ src/components/ComLogin.vue
@@ -13,8 +13,36 @@
 </template>
 
 <script>
+import users from '@/data/users';
+import eventBus from '@/eventBus';
+
 export default {
-  // Không thay đổi phần logic
+  data() {
+    return {
+      username: '',//an
+      password: '',//1234
+      error: '',
+      users:users
+    };
+  },
+ methods:{
+  login()
+  {
+    //kiem tra trong data có tức là đăng ký thành công và đăng nhập thành công
+    const user=this.users.find(x=>x.username===this.username && x.password===this.password)
+    if (user)
+    {
+      //đăng nhập thành công thì lưu đối tượng xuống localstorage
+      localStorage.setItem('curentUser',JSON.stringify(user))
+      eventBus.emit('loginSuccess', user)
+      this.$router.push('/')
+    }
+    else
+    {
+      this.error="Đăng nhập không thành công"
+    }
+  }
+ }
 };
 </script>
 
